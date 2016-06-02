175c7f05771f7702694a2d99d8917ea776fda9ea
Chris Beer <cabeer@stanford.edu>
2016-06-02 09:21:36 -0700

    Pass placement metadata through to solr for rule-based replication

--- 6.0/wrapper.sh
+++ 6.0/wrapper.sh
@@ -2,8 +2,9 @@
 
 : ${PUBLIC_PORT:="80"}
 : ${PUBLIC_HOST:=`wget -qO- http://169.254.169.254/latest/meta-data/hostname`}
+: ${PLACEMENT:=`wget -qO- http://169.254.169.254/latest/placement/availability-zone`}
 
 chown -R $SOLR_USER:$SOLR_USER $SOLR_HOME
 cp -n /opt/solr/server/solr/solr.xml $SOLR_HOME
 
-sudo -E -u $SOLR_USER /opt/solr/bin/solr start -f -h $PUBLIC_HOST -c -z $ZOOKEEPER_HOSTS -s $SOLR_HOME -a "-DhostPort=$PUBLIC_PORT"$@
+sudo -E -u $SOLR_USER /opt/solr/bin/solr start -f -h $PUBLIC_HOST -c -z $ZOOKEEPER_HOSTS -s $SOLR_HOME -a "-DhostPort=$PUBLIC_PORT -Dplacement=$PLACEMENT"$@
